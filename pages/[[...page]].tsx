f4348383ba0de1c548374ba47aa0aead8b0fe09f
Aziz Abbas <aziz@builder.io>
2022-02-16 15:38:45 -0800

    first pass

---
+++ pages/[[...page]].tsx
@@ -0,0 +1,81 @@
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
+import { BuilderComponent, Builder, builder } from '@builder.io/react'
+import DefaultErrorPage from 'next/error'
+import Head from 'next/head'
+import builderConfig from '@config/builder'
+import '@builder.io/widgets'
+import { getTargetingValues } from '@builder.io/personalization-utils';
+import { isPersonalizedPath } from '@builder.io/personalization-utils/dist/get-personalized-rewrite'
+
+
+builder.init(builderConfig.apiKey)
+
+
+// /;urlPath=, domain=, city=, 
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<{ page: string[] }>) {
+  const urlPath =  '/' + (params?.page?.join('/') || '');
+  const isPersonalized = isPersonalizedPath(urlPath);
+  const targeting = isPersonalized ? getTargetingValues(params?.page || []) : { urlPath }
+  const page =
+    (await builder
+      .get('page', {
+        userAttributes: targeting,
+      })
+      .toPromise()) || null
+
+  return {
+    props: {
+      page,
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 5 seconds
+    revalidate: 5,
+  }
+}
+
+export async function getStaticPaths() {
+  const pages = await builder.getAll('page', {
+    options: { noTargeting: true },
+    omit: 'data.blocks',
+  })
+
+  return {
+    paths: pages.map((page) => `${page.data?.url}`),
+    fallback: true,
+  }
+}
+
+export default function Page({
+  page,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+  const isLive = !Builder.isEditing && !Builder.isPreviewing
+  if (!page && isLive) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <meta name="title"></meta>
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <BuilderComponent model="page" content={page} />
+    </>
+  )
+}
---
+++ pages/_middleware.ts
@@ -0,0 +1,31 @@
+import type { NextFetchEvent, NextRequest } from 'next/server'
+
+import { NextResponse } from 'next/server';
+import { getPersonalizedRewrite } from '@builder.io/personalization-utils';
+import Cookies from 'js-cookie'
+
+// { "builder.userAttributes.domain": domain , 'builder.userAttributes.city': sf }
+
+
+// /;urlPath="...";domain=domain;city=
+
+const excludededPrefixes = ['/favicon', '/api'];
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+
+  const urlPath = req.nextUrl.pathname;
+  let response = NextResponse.next();
+
+  if (!excludededPrefixes.find(prefix => urlPath.startsWith(prefix))) {
+    const destination = getPersonalizedRewrite(urlPath, {
+      ...Cookies.get(),
+      'builder.userAttributes.domain': req.headers.get('host'),
+      'builder.userAttributes.city': req.geo?.city || '',
+    });
+
+    if (destination) {
+      response = NextResponse.rewrite(destination)
+    }
+  }
+  return response;
+}
